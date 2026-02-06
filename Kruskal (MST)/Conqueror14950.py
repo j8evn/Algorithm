23a0356b7e35b62f4eb5b90d3c8dc937e484d324
j8evn <jueunjin365@gmail.com>
2026-02-06 21:50:55 +0900

    solved: 14950

---
+++ Kruskal (MST)/Conqueror14950.py
@@ -0,0 +1,34 @@
+import sys
+input= sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union_parent(a, b):
+    a= find_parent(a)
+    b= find_parent(b)
+    if a < b:
+        parent[b]= a
+    else:
+        parent[a]= b
+
+N, M, t= map(int,input().split())
+parent= [i for i in range(N+1)]
+
+A= []
+for _ in range(M):
+    a, b, cost= map(int, input().split())
+    A.append((cost, a, b))
+A.sort()
+
+total= 0
+tt= 0
+for cost, a, b in A:
+    if find_parent(a) != find_parent(b):
+        union_parent(a, b)
+        total += cost+tt
+        tt += t
+print(total)
